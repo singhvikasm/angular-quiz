540131452c588959a94efd8637044e3b5cfd20cc
Ganesh Jadhav <ganesh.jadhav@learningmate.com>
2014-10-07 18:34:01 +0530

    add templates for question types

--- app/js/controllers/quizController.js
+++ app/js/controllers/quizController.js
@@ -2,9 +2,10 @@
 ngQuizApp.controller("ngQuizController", function($scope, $http){
 	
 	$scope.questionInProgress = 0;
+	$scope.uploadForm = {'ext': null};
 	$http({
 		'method': 'GET',
-		'url' : 'config/config.json'
+		'url' : 'config/AllQuestionTypes.json'
 	})
 	.success(function(data) { $scope.config= data; window.config= $scope.config; })
 	.error(function(data) { console.error('Config Load Error'); });
@@ -34,10 +35,24 @@ ngQuizApp.controller("ngQuizController", function($scope, $http){
 		} else {
 			return '';
 		}
-		console.log(type);
 	};
 	
 	$scope.submit = function submit() {
 		alert('');
+	};
+	
+	$scope.validateUpload = function validateUpload() {
+		var ext = $scope.uploadForm.ext.name.split('.')[1];
+		if($scope.config['questionList'][$scope.questionInProgress]['acceptableExtensions'].indexOf(ext) == -1) {
+		    alert('invalid extension!');
+		} else {
+			$http({
+				'method': 'POST',
+				'url' : '/a.php',
+				'data': { 'file': $scope.uploadForm.ext}
+			})
+			.success(function(data) { console.log('File submitted');   })
+			.error(function(data) { console.error('File Submit Error'); });
+		}
 	};
 });
---
+++ app/js/services/directives.js
@@ -0,0 +1,28 @@
+ngQuizApp.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                scope.$apply(function () {
+                    scope.fileread = changeEvent.target.files[0];
+                });
+            });
+        }
+    }
+}]);
+
+ngQuizApp.directive("replacefimb", [function () {
+    return {
+    	link: function(scope, element) { 
+    		scope.text = config['questionList'][scope.questionInProgress]['Qtext'];
+    		var arr = scope.text.match(/\[answer[1-9]\]/g), count=0;
+    		while (scope.text.match(/\[answer[1-9]\]/)) {
+    			scope.text = scope.text.replace(/\[answer[1-9]\]/,'<input type=text id="' +arr[count].substring(1,arr[count].length-1) +'"/>');
+    			count++;
+    		}
+    	},
+        template: "{{text}}"
+    }
+}]);
