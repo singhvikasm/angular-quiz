b9b814426fe33cb8fef4b42ab2cdf633e12bcec4
vikassingh <vikas.singh@learningmate.com>
2014-10-08 15:22:45 +0530

    sanitize and bind-html

--- app/js/controllers/quizController.js
+++ app/js/controllers/quizController.js
@@ -1,5 +1,5 @@
 
-ngQuizApp.controller("ngQuizController", function($scope, $http){
+ngQuizApp.controller("ngQuizController", function($scope, $http, $sce){
 	
 	$scope.questionInProgress = 0;
 	$scope.uploadForm = {'ext': null};
@@ -7,18 +7,30 @@ ngQuizApp.controller("ngQuizController", function($scope, $http){
 		'method': 'GET',
 		'url' : 'config/AllQuestionTypes.json'
 	})
-	.success(function(data) { $scope.config= data; window.config= $scope.config; })
+	//.success(function(data) { $scope.config= data; window.config= $scope.config; })
+	.success(function(data) { 
+			$scope.config= data; 
+			$scope.unsafeHtml();
+			window.config= $scope.config; 
+		})
 	.error(function(data) { console.error('Config Load Error'); });
-	
-	
+		
 	$scope.nextQuestion = function nextQuestion() {
 		//$scope.$apply(function(){  });
 		$scope.questionInProgress++;
+		$scope.unsafeHtml();		
 	};
 	
 	$scope.prevQuestion = function prevQuestion() {
 		$scope.questionInProgress--;
+		$scope.unsafeHtml();
 	};
+	
+	$scope.unsafeHtml = function unsafeHtml(){
+		if($scope.config['questionList'][$scope.questionInProgress]['Qtext']){$scope.trustedHtmlQText = $sce.trustAsHtml($scope.config['questionList'][$scope.questionInProgress]['Qtext'])}		
+		//if($scope.config['questionList'][$scope.questionInProgress]['labels']){$scope.trustedHtmllabels = $sce.trustAsHtml($scope.config['questionList'][$scope.questionInProgress]['labels'])}
+		//if($scope.config['questionList'][$scope.questionInProgress]['options']){$scope.trustedHtmloptions = $sce.trustAsHtml($scope.config['questionList'][$scope.questionInProgress]['options'])}
+	} 
 	
 	$scope.ansSelect = function ansSelect(optionID) {
 		var selectedOptionID = $scope.config['questionList'][$scope.questionInProgress]['selectedOptionID'],
